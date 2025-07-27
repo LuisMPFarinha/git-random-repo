e08e8f27b598371a064d86f0d62ca995f6023b8a
Luís Farinha <luismpfarinha@gmail.com>
2025-07-27 20:24:42 +0100

    done

--- src/components/GitProject/RepoCard.tsx
+++ src/components/GitProject/RepoCard.tsx
@@ -2,7 +2,7 @@ import { BadgeAlert, GitFork, Globe, Star } from 'lucide-react'
 
 const RepoCard = ({ repo }: { repo: any }) => {
   return (
-    <div className="relative flex flex-col w-full h-full bg-white border-2 rounded-2xl py-2 px-3 gap-1">
+    <div className="relative flex flex-col w-full h-full bg-white border-2 rounded-2xl py-2 px-3 gap-1 min-h-30">
       <label className="font-semibold">{repo.data?.name}</label>
       <label className="text-gray-500 text-sm mb-7">{repo.data?.description}</label>
       <div className="absolute flex bottom-2 w-full justify-around -ml-3 text-xs">
