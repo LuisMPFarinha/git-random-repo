73377e55d1c4a737b1f37c6235d0c01fe8eaa551
Luís Farinha <luismpfarinha@gmail.com>
2025-07-28 00:47:59 +0100

    done

--- src/hooks/useRepoContext.ts
+++ src/hooks/useRepoContext.ts
@@ -1,8 +1,9 @@
-import { RepoContext } from "@/context/RepoContext"
-import { useContext } from "react"
+import { RepoContext } from '@/context/RepoContext'
+import { useContext } from 'react'
 
 export const useRepoContext = () => {
-  const context = useContext(RepoContext);
-  if(!context) throw new Error('useRepoContext must be used within a RepoContext.Provider')
-    return context;
+  const context = useContext(RepoContext)
+  if (!context)
+    throw new Error('useRepoContext must be used within a RepoContext.Provider')
+  return context
 }
